637d514085ce55c7efcc42aaca1e382eaa92a0b9
Aaron Johnson <aaron.johnson@crowdstrike.com>
2024-03-28 17:36:53 -0500

    added FHA mortgage rates

--- main.go
+++ main.go
@@ -19,26 +19,35 @@ var (
 		Name: "mortgage30us",
 		Help: "30-Year Fixed Rate Mortgage Average in the United States",
 	})
+	fhaMortgage30Metric = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "fha_mortgage30",
+		Help: "30-Year Fixed Rate FHA Mortgage Index",
+	})
 	whitespaceRegex = regexp.MustCompile(`\s+`)
 )
 
 func fetchAndParseMortgageData() {
-	resp, err := http.Get("https://fred.stlouisfed.org/data/MORTGAGE30US.txt")
+	fetchAndParseData("https://fred.stlouisfed.org/data/MORTGAGE30US.txt", mortgage30USMetric)
+	fetchAndParseData("https://fred.stlouisfed.org/data/OBMMIFHA30YF.txt", fhaMortgage30Metric)
+}
+
+func fetchAndParseData(url string, metric prometheus.Gauge) {
+	resp, err := http.Get(url)
 	if err != nil {
-		log.Printf("Failed to fetch data: %v", err)
+		log.Printf("Failed to fetch data from %s: %v", url, err)
 		return
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		log.Printf("Failed to fetch data: HTTP status %d", resp.StatusCode)
+		log.Printf("Failed to fetch data from %s: HTTP status %d", url, resp.StatusCode)
 		return
 	}
 
-	parseMortgageData(resp.Body)
+	parseMortgageData(resp.Body, metric)
 }
 
-func parseMortgageData(reader io.Reader) {
+func parseMortgageData(reader io.Reader, metric prometheus.Gauge) {
 	scanner := bufio.NewScanner(reader)
 	inDataSection := false
 	for scanner.Scan() {
@@ -50,13 +59,13 @@ func parseMortgageData(reader io.Reader) {
 
 		if inDataSection {
 			parts := whitespaceRegex.Split(line, -1)
-			if len(parts) == 2 {
+			if len(parts) == 2 && parts[1] != "." {
 				value, err := strconv.ParseFloat(parts[1], 64)
 				if err != nil {
 					log.Printf("Failed to parse value: %v", err)
 					continue
 				}
-				mortgage30USMetric.Set(value)
+				metric.Set(value)
 				log.Printf("Value: %.2f", value)
 			}
 		}
